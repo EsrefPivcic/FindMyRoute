c9bb1d2a60ef5b058b16248b94fb781705ce47eb
Nedžmija Muminović <nedzmija.muminovic@edu.fit.ba>
2023-07-04 14:34:02 +0200

    Popravke u dizajnu

--- angular_app/src/app/app.component.ts
+++ angular_app/src/app/app.component.ts
@@ -5,7 +5,7 @@ import {Router} from "@angular/router";
 import {AutentifikacijaHelper} from "./_helpers/autentifikacija-helper";
 import {LoginInformacije} from "./_helpers/login-informacije";
 
-declare function porukaSuccess(a: string):any;
+declare function porukaSuccess(a: string, options?: any): any;
 
 @Component({
   selector: 'app-root',
@@ -63,7 +63,7 @@ export class AppComponent implements OnInit {
     AutentifikacijaHelper.setLoginInfo(null);
     this.httpKlijent.post(MojConfig.adresa_servera + "/Autentifikacija/Logout/", null, token)
       .subscribe((x: any) => {
-        porukaSuccess("Logout uspješan");
+        porukaSuccess("Logout uspješan", { position: 'top-left' });
       });
     this.router.navigateByUrl("/login");
   }
